e385483a69201fdefaeb2b3feea09f1ca9183ad6
Vaibhav <vaibhav4gahat@gamil.com>
2023-08-01 14:25:53 +0530

    Remove gh-pages from package.json

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Social from './components/Social';
-import About from './components/About';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Social from "./components/Social";
+import About from "./components/About";
 // import Portfolio from './components/Portfolio';
-import Skill from './components/Skill';
-import Contact from './components/Contact';
-
+import Skill from "./components/Skill";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-<>
-<Navbar></Navbar>
-<Home></Home>
-<Social/>
-<About></About>
-{/* <Portfolio/>  */}
-<Skill/>
-<Contact/>
-
-
-
-</>
+    <>
+      <Navbar></Navbar>
+      <Home></Home>
+      <Social />
+      <About></About>
+      {/* <Portfolio/>  */}
+      <Skill />
+      <Contact />
+    </>
   );
 }
 
